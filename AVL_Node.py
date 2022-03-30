c5e1ef6a385055cb4c746cf969ff80ca5d2d33d8
Lunahey <55185494+Lunahey@users.noreply.github.com>
2022-03-30 12:56:30 +0200

    Refactorisation code

--- AVL_Node.py
+++ AVL_Node.py
@@ -1,11 +1,11 @@
-from readline import insert_text
-
+import unittest
+NB_ROT = 0
+def reset_nb_rot():
+    global NB_ROT
+    NB_ROT = 0
 
 class AVL_Node:
-    NB_ROT = 0
-    def reset_nb_rot():
-        global NB_ROT
-        NB_ROT = 0
+
 
     def __init__(self, value: int) -> None:
         self._value = value
@@ -27,14 +27,7 @@ class AVL_Node:
                 node_right, b = self._right.insert_wrapper(value)
                 self._balance = self._balance - b
                 if (self._balance == -2):
-                    if node_right._balance > 0: # désequilibre à droite
-                        node_right._right = node_right._left
-                        node_right._left = None
-                    node_right._left = self
-                    self._right = None
-                    node_right._balance = 0
-                    self._balance = 0
-                    return (node_right, 0)
+                   return self.rotation_left(node_right), 0
 
         elif self._value > value:
             if self._left == None:
@@ -46,13 +39,29 @@ class AVL_Node:
                 node_left, b = self._left.insert_wrapper(value)
                 self._balance = self._balance + b
                 if (self._balance == 2):
-                    if node_left._balance < 0: # désequilibre à droite
-                        node_left._left = node_left._right
-                        node_left._right = None
-                    node_left._right = self
-                    self._left = None
-                    node_left._balance = 0
-                    self._balance = 0
-                    return(node_left, 0)
+                    return self.rotation_right(node_left), 0
         return self, 0
 
+    def rotation_left(self, node_right):
+        if node_right._balance > 0: # désequilibre à droite
+            node_right._right = node_right._left
+            node_right._left = None
+        node_right._left = self
+        self._right = None
+        node_right._balance = 0
+        self._balance = 0
+        return node_right
+
+    def rotation_right(self, node_left):
+        if node_left._balance < 0: # désequilibre à droite
+            node_left._left = node_left._right
+            node_left._right = None
+        node_left._right = self
+        self._left = None
+        node_left._balance = 0
+        self._balance = 0
+        return(node_left)
+
+    def turn_left(self):
+        assert (self._right is not None)
+        
